5a4b15596bba0f95b92b9d9330965e70bd333539
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-21 02:34:41 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,5 @@
 
 def fizzbuzz(num)
   
-  if num % 15 == 0
-    return "FizzBuzz"
-  elsif num % 3 == 0
-    return "Fizz"
-  elsif num % 5 == 0
-    return "Buzz"
-  else
-    return nil 
-    
- end
+
 end
